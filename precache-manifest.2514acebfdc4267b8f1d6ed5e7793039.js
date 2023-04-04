08697fca4b7bb5ec1c85d0b3e14a4b711d4b4add
Paulo <paulomga10@gmail.com>
2023-04-03 21:15:22 -0500

    Updates

---
+++ precache-manifest.2514acebfdc4267b8f1d6ed5e7793039.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ac6c549bc156d1b2721908179001321",
+    "url": "/RickAndMortyAPI/index.html"
+  },
+  {
+    "revision": "4273211731271da3a282",
+    "url": "/RickAndMortyAPI/static/css/main.27673896.chunk.css"
+  },
+  {
+    "revision": "994f3ad5629b6d9c2990",
+    "url": "/RickAndMortyAPI/static/js/2.9abc5be5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/RickAndMortyAPI/static/js/2.9abc5be5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4273211731271da3a282",
+    "url": "/RickAndMortyAPI/static/js/main.9b1a0293.chunk.js"
+  },
+  {
+    "revision": "72e2ed6a1e389d60bbbc",
+    "url": "/RickAndMortyAPI/static/js/runtime-main.c906b13e.js"
+  }
+]);
